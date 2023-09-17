3be88f2ba9b395ab63a88205b54d62242a112e70
narayanab16 <narayanab16@gmail.com>
2023-09-17 17:09:38 -0500

    kafka streams using kstreams libray

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# kafkastreams
-kafka streams using kstreams library
+# Kafka streams using kstreams library
+
+- Kafka streams used to build a Topology and Sub Topology of data transformations 
+like aggregations, KStream, KTable, state of the data etc
+# Kafka Console Producer, and Console Consumer 
+
+-    Prereq: Docker, DockerCompose, IntelliJ, Java 8+, Here i use JDK 18/20
+-    And or Any Local kafka broker running
+-    Start your confluent kafka docker (see under resources)
+-     [admin@fedora37 ~]$ docker-compose -f confluent-kafka.yml up 
+-    Create a topic (see under resources/kafka-help.txt)
+-    Receive data from input topic(dataIn4), transform , send to the output topic(dataOut)
+
+
---
+++ src/main/java/com/narayana/kafkastreams/consumer/ApplicationKStreamExample.java
@@ -0,0 +1,75 @@
+package com.narayana.kafkastreams.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.state.KeyValueStore;
+
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+public class ApplicationKStreamExample {
+
+    public static void main(final String[] args) {
+        final String bootstrapServers = args.length > 0 ? args[0] : "localhost:9092";
+        final Properties streamsConfiguration = new Properties();
+        streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "application-kstream-demo");
+        streamsConfiguration.put(StreamsConfig.CLIENT_ID_CONFIG, "application-kstream-demo-client");
+        streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        streamsConfiguration.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        final boolean doReset = args.length > 1 && args[1].equals("--reset");
+        final KafkaStreams streams = buildKafkaStreams(streamsConfiguration);
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+        // Delete the application's local state on reset
+        if (doReset) {
+            streams.cleanUp();
+        }
+        startKafkaStreamsSynchronously(streams);
+    }
+
+    static KafkaStreams buildKafkaStreams(final Properties streamsConfiguration) {
+        final StreamsBuilder builder = new StreamsBuilder();
+        final KStream<String, String> input = builder.stream("dataIn4");
+        System.out.println(" buildKafkaStreams --> " + input);
+        input.flatMapValues(value -> Arrays.asList(value.split(" ")))
+                .groupBy((key, value) -> {
+                    System.out.println(" key: " + key + ", value : " + value);
+                    return value;
+                })
+                .count(Materialized.as("counts-store"))
+                .toStream().to("dataOut0", Produced.with(Serdes.String(),Serdes.Long()));
+        System.out.println(" buildKafkaStreams --> " + "out");
+
+        Topology topology = builder.build();
+        System.out.println("Kstream Topology : " + topology.describe());
+        return new KafkaStreams(topology, streamsConfiguration);
+    }
+
+    static void startKafkaStreamsSynchronously(final KafkaStreams streams) {
+        final CountDownLatch latch = new CountDownLatch(1);
+        streams.setStateListener((newState, oldState) -> {
+            System.out.println("old : " + oldState.isRunning() + ", new : " + newState.isRunning());
+            if (oldState == KafkaStreams.State.REBALANCING && newState == KafkaStreams.State.RUNNING) {
+                latch.countDown();
+            }
+        });
+        // start kstream service
+        streams.start();
+        try {
+            latch.await();
+        } catch (final InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
